baf5e8dbc8c470d152dbc6ad67fe0ec3735622df
wojciech Marek <wojtekmarek@gmail.com>
2023-09-04 15:00:26 +0200

    c3a

--- src/main/java/tacos/data/IngredientRepository.java
+++ src/main/java/tacos/data/IngredientRepository.java
@@ -2,7 +2,7 @@ package tacos.data;
 import org.springframework.data.repository.CrudRepository;
 import tacos.Ingredient;
 
-public interface IngredientRepository extends CrudRepository<Ingredient, String> {
+public interface IngredientRepository  {
     Iterable<Ingredient> findAll();
     Ingredient findById(String id);
     Ingredient save(Ingredient ingredient);
