77ba9baf0845547a837043473cebb239818165bd
wojciech Marek <wojtekmarek@gmail.com>
2023-09-05 11:00:15 +0200

    c4

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package tacos.security;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,4 +10,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+        auth
+                .inMemoryAuthentication()
+                .withUser("buzz")
+                .password("{noop}infiinity")
+                .authorities("ROLE_USER")
+                .and()
+                .withUser("buzzo")
+                .password("{noop}inf")
+                .authorities("ROLE_USER");
+
+    }
 }
